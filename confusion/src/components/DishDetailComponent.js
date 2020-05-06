6f193bf189462ce1694e4ee66adf129a81f768ef
yrshkhn <sergeyyershykhin@gmail.com>
2020-05-07 00:11:05 +0300

    added DishDetail component and completed Week 1 of course

---
+++ confusion/src/components/DishDetailComponent.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+
+import {Card, CardImg, CardText, CardTitle, CardBody} from 'reactstrap';
+
+class DishDetail extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+        }
+    }
+    
+    renderDish(dish){
+        if(dish != null){
+            return(
+                <Card>
+                    <CardImg top src = {dish.image} alt = {dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(comments){
+        if(comments != null){
+            return(
+                <div>
+                    <h4>Comments</h4>
+                        {comments}
+                </div>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    render(){
+        let comments = null;
+        if(this.props.dish!=null){
+            comments = this.props.dish.comments.map((comment) => {
+                return(
+                    <li key = {comment.id} className="list-unstyled">
+                        <p>{comment.comment}</p>
+                        <p>{comment.author} , {comment.date}</p>
+                    </li>
+                );
+            });
+        }
+        return(
+            <div className = "row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(comments)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-
-import {Card, CardImg, CardImgOverlay, CardText, CardTitle, CardBody} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardText, CardTitle} from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component{
     constructor(props){
@@ -13,25 +13,6 @@ class Menu extends Component{
 
     onDishSelect(dish){
         this.setState({selectedDish:dish});
-    }
-
-    renderDish(dish){
-        if(dish != null){
-            return(
-                <Card>
-                    <CardImg top src = {dish.image} alt = {dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else{
-            return(
-                <div></div>
-            );
-        }
     }
 
     render(){
@@ -53,57 +34,10 @@ class Menu extends Component{
                 <div className = "row">
                     {menu}
                 </div>
-                <div className = "row">
-                    <div className="col-Я col-md-5 m-1">
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                </div>
+                    <DishDetail dish = {this.state.selectedDish}/>
             </div>
         );
     }
 }
 
 export default Menu;
-
-/*import {Media} from 'reactstrap';
-
-class Menu extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            
-        }
-    }
-
-    render(){
-        const menu = this.props.dishes.map((dish) => {
-            return(
-                <div key = {dish.id} className = "col-12 mt-5">
-                    <Media tag = "li">
-                        <Media left middle>
-                            <Media object src = {dish.image} alt = {dish.name}/>
-                        </Media>
-                        <Media body className = "ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
-                </div>
-            );
-        });
-
-        return (
-            <div className = "container">
-                <div className = "row">
-                    <Media list>
-                        {menu}
-                    </Media>
-                </div>
-            </div>
-        );
-    }
-}
-export default Menu;
-
-*/
