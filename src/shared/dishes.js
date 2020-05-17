1819487edf9c71f70134e69f325174b785b057c5
yrshkhn <sergeyyershykhin@gmail.com>
2020-05-17 17:37:46 +0300

    minor updates

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -3,7 +3,7 @@ export const DISHES =
         {
         id: 0,
         name:'Uthappizza',
-        image: '/assets/images/uthappizza.png',
+        image: './assets/images/uthappizza.png',
         category: 'mains',
         label:'Hot',
         price:'4.99',
