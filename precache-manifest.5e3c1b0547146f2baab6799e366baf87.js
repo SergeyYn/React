780bbcefef2d0847abd09a428273b18e29d6b8d7
yrshkhn <sergeyyershykhin@gmail.com>
2020-05-17 16:08:12 +0300

    Updates

---
+++ precache-manifest.5e3c1b0547146f2baab6799e366baf87.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ac7776fc9f3f9b68cf3764af6ca553",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "7b6041db864d57795719",
+    "url": "/React/static/css/2.9bbd6f83.chunk.css"
+  },
+  {
+    "revision": "86e81dd7006ebb3ac178",
+    "url": "/React/static/css/main.2e1c626b.chunk.css"
+  },
+  {
+    "revision": "7b6041db864d57795719",
+    "url": "/React/static/js/2.1c60549f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React/static/js/2.1c60549f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e81dd7006ebb3ac178",
+    "url": "/React/static/js/main.36a267b2.chunk.js"
+  },
+  {
+    "revision": "79e5e1c21761871596e0",
+    "url": "/React/static/js/runtime-main.396133cc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
